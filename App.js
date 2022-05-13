3a5567bb72bb03e4776bbd6109a9879c3cc5e58a
Mariana de Andrade Correa <marianacorrea@bb.com.br>
2022-05-13 15:01:30 -0300

    Exemplos da aula 1

---
+++ App.js
@@ -0,0 +1,68 @@
+// Tipo number
+
+const MEU_NUMERO = 42;
+const FIRST = 21;
+const SECOND = 21;
+
+const SUM = FIRST - SECOND;
+
+console.log(SUM);
+
+// Ponto flutuante
+
+const PONTO_FLUTUANTE = .42;
+
+const TESTEPONTOFLUTUANTE = MEU_NUMERO - PONTO_FLUTUANTE;
+
+console.log(TESTEPONTOFLUTUANTE);
+
+// Not a Number - NaN
+
+const WHAT = "Alura";
+
+console.log(PONTO_FLUTUANTE - SECOND * WHAT);
+
+// Infinity
+
+var a = 10;
+var b = 0;
+console.log(a/b);
+
+// Tipo String
+
+const TEXT1 = "MariIii";
+const TEXT2 = "12345";
+const TEXT3 = 'Veja só';
+const TEXT4 = 'A Mari disse: '
+
+console.log(TEXT4 + TEXT3);
+
+
+// CÓDIGO UNICODE
+
+const cifrao = '\u01A2'
+const aMaiusculo = '\u229B'
+const tique = '\u2705'
+const hiragana = '\u3041'
+
+console.log(cifrao)
+console.log(aMaiusculo)
+console.log(tique)
+console.log(hiragana)
+
+// TO LOWER CASE
+
+console.log(TEXT1.toLowerCase());
+console.log(TEXT1.toUpperCase());
+
+// LENGHT
+
+console.log(TEXT1.length);
+
+// BOOLEAN
+
+const V1 = 42
+const V2 = 21
+
+console.log(V1/2 === V2);
+
